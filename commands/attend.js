b62ca58114127562877995241926dad8304d6112
toplordsaito <42961261+toplordsaito@users.noreply.github.com>
2020-06-19 19:04:24 +0700

    Update attend.js

--- commands/attend.js
+++ commands/attend.js
@@ -1,13 +1,23 @@
 const fs = require('fs');
 let rawdata = fs.readFileSync('pair.json');
 let pair = JSON.parse(rawdata);
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
     name: 'เช็คชื่อ',
     description: 'check attend',
-    execute(msg, args) {
+    async execute(msg, args) {
         if (!msg.member.hasPermission("ADMINISTRATOR", true))
             return;
+
+        var embed = new MessageEmbed()
+            .setImage(msg.channel.guild.iconURL)
+            .setColor('RANDOM')
+            .setAuthor('ข้อมูลการเช็คชื่อ', msg.channel.guild.iconURL)
+            .setTimestamp()
         const channels = msg.guild.channels.cache.filter(c => c.type === 'voice');
+        let come = []
+        let upset = []
         for (const [channelID, channel] of channels) {
             let room = pair.find(p => {
                 return `#${p.room < 10 ? '0' + p.room : p.room} - Lab` == channel.name
@@ -15,11 +25,20 @@ module.exports = {
             if (!room) continue
             for (i = 0; i < room.member.length; i++) {
                 if (channel.members.some(m => m.nickname.split(" ")[0] === room.member[i])) {
-                    msg.channel.send(`${room.member[i]} มา`)
+                    come.push(room.member[i])
                 } else {
-                    msg.channel.send(`${room.member[i]} ขาด`)
+                    upset.push(room.member[i])
                 }
             }
         }
+        if (come.length === 0) {
+            come.push('_')
+        } if (upset.length === 0) {
+            upset.push("_")
+        }
+        await embed.addField("มา", come)
+        await embed.addField("ขาด", upset)
+        await msg.reply(embed).catch()
+
     },
 };
